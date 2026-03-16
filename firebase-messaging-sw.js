de493f5249a7ae847dbdf9fc7f09e0fdb6fd0be1
saif heny <sayfhenyi2@gmail.com>
2026-03-17 01:03:00 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,26 @@
-// جلب مكتبات Firebase الخاصة بعامل الخدمة
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-// يجب وضع نفس الـ Config هنا أيضاً لكي يعمل في الخلفية
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAjE-2q6PONBkCin9ZN22gDp9Q8pAH9ZW8",
+    authDomain: "story-97cf7.firebaseapp.com",
+    databaseURL: "https://story-97cf7-default-rtdb.firebaseio.com",
+    projectId: "story-97cf7",
+    storageBucket: "story-97cf7.firebasestorage.app",
+    messagingSenderId: "742801388214",
+    appId: "1:742801388214:web:32a305a8057b0582c5ec17"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// استقبال الإشعار عندما يكون الموقع مغلقاً (Background)
 messaging.onBackgroundMessage((payload) => {
-    console.log("تم استقبال رسالة في الخلفية: ", payload);
+    console.log("تم استقبال إشعار في الخلفية: ", payload);
 
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon.png' // يمكنك وضع مسار أي أيقونة لديك
+        icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png' // أيقونة افتراضية للرسالة
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
